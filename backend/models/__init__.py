483f82f402e5a5bb9b789eb106e6615381173d01
Jain CK <kuriakosejain456@gmail.com>
2025-06-15 19:53:19 +0530

    model restructure

---
+++ backend/models/__init__.py
@@ -0,0 +1,19 @@
+from .database import ComparisonHistory
+from .schemas import (
+    ComparisonRequest,
+    ComparisonResponse,
+    ComparisonHistoryResponse,
+    SkillMatch,
+    HealthResponse,
+    ErrorResponse
+)
+
+__all__ = [
+    "ComparisonHistory",
+    "ComparisonRequest", 
+    "ComparisonResponse",
+    "ComparisonHistoryResponse",
+    "SkillMatch",
+    "HealthResponse",
+    "ErrorResponse"
+]
